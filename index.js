601ff3fe5bc5cf6450c8ffe48fa1e3c59f8d260d
Stefano Saccucci <stefano.saccucci@gmail.com>
2018-09-19 11:38:14 +0200

    remove inquirer

--- index.js
+++ index.js
@@ -16,63 +16,21 @@ config.node.rpcHandlers = [
 var eventEmitter = new events.EventEmitter();
 
 // Use standard UQ node factory to create an UQ Node:   
-standardUQNodeFactory(config.node).then(uq => {
-        console.log('MY NAME IS :', uq.nodename);
+standardUQNodeFactory(config.node)
+    .then(uq => {
+        console.log('MY NAME IS:', uq.nodename);
 
-        setInterval(function(){
-            console.log("I'm looking for a contract with", config.aws.awsNode);
-            var contract = uq.db.findUserContractsByProviderName(config.aws.awsNode); //to set AWS name
-            console.log(contract);
-            contract = ["ciaoooooookjhaefjkhajhf"]
-            eventEmitter.emit('locked', contract);
+        var si = setInterval(function(){
+            console.log("I'm looking for a contract with", config.aws.awsNode); //QUI
+            var contract = uq.db.findUserContractsByProviderName(config.aws.awsNode); //QUI
+            contract = ['XXX']
+            if(contract.length>0){
+                eventEmitter.emit('locked', contract, uq);
+            }
         }, 5000);
     }, error => {
         console.log(error);
 })
-
-
-
-
-
-
-/*cli = function(uq){
-    inquirer.prompt(  {
-        type: 'confirm',
-        name: 'startup',
-        message: "Do you want try to connect to AWS IoT?",
-        default: true
-    }).then(answers => {
-        if(answers.startup == true) {
-        inquirer.prompt({
-            type: 'input',
-            name: 'aws',
-            message: "What's the AWS IoT name?",
-            default: function() {
-            return 'AWS-JSF54EB8A2C913';
-            }
-        }).then(answers => {
-                       
-            if( contract.length>0 && contract[0].identity.role == 'USER') {
-            
-            var _timestamp = Math.floor(new Date()/1000)
-            var b_timestamp = Buffer.from(_timestamp.toString(), 'utf8');
-            var b_signed = uq.id.signFor({role: contract[0].identity.role, index: contract[0].identity.index, ext: contract[0].identity.ext}, b_timestamp)
-            config.aws.cauth.clientId = uq.nodename;
-            config.aws.cauth.customAuthHeaders['x-amz-customAuthorizer-name'] = config.aws.cauth.authorizerName;
-            awsDevice(config.aws.tokenKey, config.aws.cauth, config.aws.keyFile, JSON.stringify({
-                useraddress:contract[0].identity.address,
-                timestamp:_timestamp,
-                signature:b_signed.toString('base64')
-            }));
-            
-        
-            } else {
-                console.log("There is not a valid contract.");
-            }
-        });
-        }
-    });
-}*/
 
 awsDevice = function(tokenkey, options, keyfile, token){
     var pem = fs.readFileSync(keyfile);
@@ -92,7 +50,7 @@ awsDevice = function(tokenkey, options, keyfile, token){
             
     device.on('message', function(topic, payload) {
         console.log('message', topic, payload.toString());
-        });
+    });
 
     device.on('error', function(error) {
         console.log('error', error);
@@ -115,12 +73,21 @@ awsDevice = function(tokenkey, options, keyfile, token){
 
 }
 
-eventEmitter.on('locked', function(contract){
+eventEmitter.on('locked', function(contract, uq){
     console.log(contract)
     if( contract.length>0 && typeof contract[0].identity != 'undefined' && typeof contract[0].identity.role != 'undefined' &&  contract[0].identity.role == 'USER') {
-        console.log("There is a valid contract. I'm connecting to", answers.aws); //
-
+        console.log("There is a valid contract. I'm connecting to", answers.aws); // QUI
     } else {
+        var _timestamp = Math.floor(new Date()/1000)
+        var b_timestamp = Buffer.from(_timestamp.toString(), 'utf8');
+        //var b_signed = uq.id.signFor({role: contract[0].identity.role, index: contract[0].identity.index, ext: contract[0].identity.ext}, b_timestamp)
+        config.aws.cauth.clientId = uq.nodename;
+        config.aws.cauth.customAuthHeaders['x-amz-customAuthorizer-name'] = config.aws.cauth.authorizerName;
+        awsDevice(config.aws.tokenKey, config.aws.cauth, config.aws.keyFile, JSON.stringify({
+            userAddress: "n36Gbf8y6A52FFCE1Dz6yX6SiBmG5qgF47",//contract[0].identity.address,
+            timestamp:_timestamp,
+            signature: b_timestamp//b_signed.toString('base64')
+        }));;
         console.log("There is not a valid contract.");
     }
 });
