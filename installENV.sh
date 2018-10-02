183ea12eadbd24e849b13b3b2d9d32df043acd0c
stefacc <stefano.saccucci@gmail.com>
2018-10-02 08:41:48 +0200

    Update installENV.sh

--- installENV.sh
+++ installENV.sh
@@ -1,1 +1,1 @@
-sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - || sudo apt-get install -y nodejs || sudo apt-get install -y cmake || sudo apt-get install -y make || sudo apt-get install -y g++
+sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - ; sudo apt-get install -y nodejs ; sudo apt-get install -y cmake ; sudo apt-get install -y make ; sudo apt-get install -y g++
