08e2d760213b5739fc7590bfed123b12b7541768
Stefano Saccucci <stefano.saccucci@gmail.com>
2018-10-04 15:00:23 +0200

    add copyright lines

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
+/**!
+ *
+ * Copyright 2016-2018 Uniquid Inc. or its affiliates. All Rights Reserved.
+ *
+ * License is in the "LICENSE" file accompanying this file.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
 var config = { aws:{}, node:{} };
 
 config.aws = {
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/**!
+ *
+ * Copyright 2016-2018 Uniquid Inc. or its affiliates. All Rights Reserved.
+ *
+ * License is in the "LICENSE" file accompanying this file.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
 const argv = require('minimist')(process.argv.slice(2));
 console.log(argv);
 var config
